acc2eb8223e44ad3e6175f4cf0d04d74fe9de164
OliverKarlsson <o.a.n.karlsson@gmail.com>
2023-04-21 23:07:11 +0200

    add a hidden section for screen readers to the app

--- employees-fe/src/App.tsx
+++ employees-fe/src/App.tsx
@@ -3,12 +3,30 @@ import EmployeesTool from "components/EmployeesTool/EmployeesTool";
 import useEmployees from "hooks/useEmployees";
 
 const App = () => {
-  const { employees, activeView, ...rest } = useEmployees();
+  const { employees, activeView, activeFilter, activeSort, ...rest } =
+    useEmployees();
 
   return (
     <>
-      <EmployeesTool activeView={activeView} {...rest} />
-      <EmployeeView activeView={activeView} employees={employees} />
+      <EmployeesTool
+        activeFilter={activeFilter}
+        activeSort={activeSort}
+        activeView={activeView}
+        {...rest}
+      />
+      <main>
+        <section className="visually-hidden">
+          <h2>View configuration summary</h2>
+          <p>The active sort is set to {activeSort}</p>
+          {activeFilter && (
+            <p>There exists a filter on the word: "{activeFilter}"</p>
+          )}
+          <p>The active view is set to {activeView}</p>
+        </section>
+        <section>
+          <EmployeeView activeView={activeView} employees={employees} />
+        </section>
+      </main>
     </>
   );
 };
