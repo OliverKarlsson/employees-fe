7c9bc9de58d0511e6e9ef4374af3c27aa5b3eb03
OliverKarlsson <o.a.n.karlsson@gmail.com>
2023-04-21 20:42:27 +0200

    add employees tool & views to the app

--- employees-fe/src/App.tsx
+++ employees-fe/src/App.tsx
@@ -1,35 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import EmployeeGrid from "components/EmployeeGrid/EmployeeGrid";
+import EmployeeList from "components/EmployeeList/EmployeeList";
+import EmployeesTool from "components/EmployeesTool/EmployeesTool";
+import useEmployees from "hooks/useEmployees";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const { employees, activeView, ...rest } = useEmployees();
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
-}
+    <>
+      <EmployeesTool activeView={activeView} {...rest} />
+      {activeView === "grid" && <EmployeeGrid employees={employees} />}
+      {activeView === "list" && <EmployeeList employees={employees} />}
+    </>
+  );
+};
 
-export default App
+export default App;
